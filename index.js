a36df00c3bfd88b5bb8bc9bba7ee220ce8d4f7e1
Frank Trevino <fjoshuat@gmail.com>
2022-11-07 15:19:51 -0600

    commit tree 66016e68cc979c17c125622d7e5c58d5a92b8838

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+return cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+    return cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+
+function appendCat(Broom) {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(Arnold) {
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice();
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice();
+    copyOfCats.shift();
+    return copyOfCats;
+}
